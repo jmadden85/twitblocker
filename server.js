708c54c03b009db215dc38c2fe7c61d03fef8d3f
Joe Madden <Joe@beardbro.com>
2014-05-29 18:18:05 -0400

    added an endpoint

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ var Twit = require('twit');
 var http = require('http');
 var url = require('url');
 
-var T = new Twit({
+var tTraderoom = new Twit({
+    consumer_key:         'xxx',
+    consumer_secret:      'xxx',
+    access_token:         'xxx',
+    access_token_secret:  'xxx'
+});
+
+var tNS = new Twit({
     consumer_key:         'xxx',
     consumer_secret:      'xxx',
     access_token:         'xxx',
@@ -11,10 +18,19 @@ var T = new Twit({
 
 var server = http.createServer(function (req, res) {
     var newReq = url.parse(req.url, true);
-    if (req.method === 'POST' && newReq.pathname === '/blockuser') {
-        blockUser(newReq.query.screen_name, 'block');
-    } else if (req.method === 'POST' && newReq.pathname === '/unblockuser') {
-        blockUser(newReq.query.screen_name, 'unblock');
+    if (req.method === 'POST') {
+        switch (newReq.pathname) {
+            case '/blockuser':
+                blockUser(newReq.query.screen_name, 'block');
+                break;
+            case '/unblockuser':
+                blockUser(newReq.query.screen_name, 'unblock');
+                break;
+            case '/frienduser':
+                friendUser(newReq.query.screen_name, 'account');
+            default
+                break;
+        }
     }
     res.writeHead(200);
     res.end();
@@ -22,15 +38,16 @@ var server = http.createServer(function (req, res) {
 
 function blockUser (user, method) {
     var block = method === 'block' ? 'blocks/create' : 'blocks/destroy';
-    T.post(block,  {
+    tTraderoom.post(block,  {
         screen_name: user
     },  function (err, data, response) {
         console.log(data);
     });
 };
 
-function friendUser (user) {
-    T.post('friendships/create', {
+function friendUser (user, account) {
+    var thisAccount = account === 'traderoom' ? tTraderoom : tNs;
+    thisAccount.post('friendships/create', {
         screen_name: user,
         follow: true
     });
